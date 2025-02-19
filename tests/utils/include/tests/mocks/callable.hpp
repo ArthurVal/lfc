d5d69572511b3bd3f6b246e8907db4934860c225
Arthur Valiente <valiente.arthur@gmail.com>
2025-02-19 14:59:23 +0100

    Remove CallabaleMock specialization for single arg

--- tests/utils/include/tests/mocks/callable.hpp
+++ tests/utils/include/tests/mocks/callable.hpp
@@ -1,28 +1,44 @@
 #pragma once
 
+#include <tuple>
+#include <type_traits>
+
 #include "gmock/gmock.h"
 
 namespace tests {
 
+namespace details {
+
+template <class... T>
+struct FirstOf;
+
+template <class T, class... Others>
+struct FirstOf<T, Others...> {
+  using type = T;
+};
+
+template <class... T>
+using FirstOf_t = typename FirstOf<T...>::type;
+
+}  // namespace details
+
 template <class R, class... Args>
 struct CallableMock {
   using return_t = R;
-  using arg_t = std::tuple<Args...>;
+  using arg_t = std::conditional_t<
+      sizeof...(Args) == 0, void,
+      std::conditional_t<sizeof...(Args) == 1, details::FirstOf_t<Args...>,
+                         std::tuple<Args...>>>;
 
-  MOCK_METHOD(return_t, Call, (std::tuple<Args...>), (const));
+  MOCK_METHOD(return_t, Call, (arg_t), (const));
   constexpr auto operator()(Args... args) const -> return_t {
-    return Call(std::make_tuple(std::move(args)...));
-  }
-};
-
-template <class R, class Arg>
-struct CallableMock<R, Arg> {
-  using return_t = R;
-  using arg_t = Arg;
-
-  MOCK_METHOD(return_t, Call, (Arg), (const));
-  constexpr auto operator()(Arg arg) const -> return_t {
-    return Call(std::move(arg));
+    if constexpr (sizeof...(Args) == 0) {
+      return Call();
+    } else if constexpr (sizeof...(Args) == 1) {
+      return Call(std::move(args)...);
+    } else {
+      return Call(std::make_tuple(std::move(args)...));
+    }
   }
 };
 
