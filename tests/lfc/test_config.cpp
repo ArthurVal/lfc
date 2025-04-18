68d87d00278cf9ab803a6ffb6093000a28cd0bfa
Arthur Valiente <valiente.arthur@gmail.com>
2025-04-18 16:31:41 +0200

    Simplify the GIT version handling

--- tests/lfc/test_config.cpp
+++ tests/lfc/test_config.cpp
@@ -32,19 +32,21 @@ auto StdoutFrom(const char* cmd) -> std::optional<std::string> {
 #define STR(x) #x
 
 TEST(ConfigTest, VersionMatchesWithGit) {
-  using testing::ContainsRegex;
+  using testing::Eq;
 
+  const auto git_version =
 #ifdef PROJECT_GIT_LOCATION
-  EXPECT_THAT(
       StdoutFrom(
           "cd " XSTR(PROJECT_GIT_LOCATION) " && git describe --abbrev=0 2>&1")
-          .value_or(std::strerror(errno)),
-      ContainsRegex("v" lfc_VERSION_STR));
+          .value_or(std::strerror(errno));
 #else
-  EXPECT_THAT(
-      StdoutFrom("git describe --abbrev=0 2>&1").value_or(std::strerror(errno)),
-      ContainsRegex("v" lfc_VERSION_STR));
+      StdoutFrom("git describe --abbrev=0 2>&1").value_or(std::strerror(errno));
 #endif
+
+  EXPECT_THAT(git_version, Eq("v" lfc_VERSION_STR "\n"))
+      << "Both version must match:"
+         "\n - The version in the main CMakeLists.txt;"
+         "\n - The last git tag (annotated);";
 }
 
 }  // namespace
