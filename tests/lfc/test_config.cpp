6bc50e753bd880f5dab9b7ea0cda9c618139fdec
Arthur Valiente <valiente.arthur@gmail.com>
2025-04-18 16:02:24 +0200

    Fix the case where build dir is not inside the src dir

--- tests/lfc/test_config.cpp
+++ tests/lfc/test_config.cpp
@@ -28,11 +28,23 @@ auto StdoutFrom(const char* cmd) -> std::optional<std::string> {
   return res;
 }
 
+#define XSTR(x) STR(x)
+#define STR(x) #x
+
 TEST(ConfigTest, VersionMatchesWithGit) {
   using testing::ContainsRegex;
+
+#ifdef PROJECT_GIT_LOCATION
+  EXPECT_THAT(
+      StdoutFrom(
+          "cd " XSTR(PROJECT_GIT_LOCATION) " && git describe --abbrev=0 2>&1")
+          .value_or(std::strerror(errno)),
+      ContainsRegex("v" lfc_VERSION_STR));
+#else
   EXPECT_THAT(
       StdoutFrom("git describe --abbrev=0 2>&1").value_or(std::strerror(errno)),
       ContainsRegex("v" lfc_VERSION_STR));
+#endif
 }
 
 }  // namespace
